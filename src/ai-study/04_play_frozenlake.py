d04eacacf9e01ba77789d909d3908cebcfa8f0bc
yejishin-hub <sinyeji958@gmail.com>
2025-11-17 22:04:22 +0900

    new file:   src/ai-study/04_play_frozenlake.py Non-deterministic

---
+++ src/ai-study/04_play_frozenlake.py
@@ -0,0 +1,48 @@
+import gym
+import readchar
+
+import utils.prints as print_utils
+
+# MACROS
+LEFT = 0
+DOWN = 1
+RIGHT = 2
+UP = 3
+
+# Key mapping
+arrow_keys = {
+    '\x1b[A': UP, 
+    '\x1b[B': DOWN, 
+    '\x1b[C': RIGHT, 
+    '\x1b[D': LEFT}
+
+## Deprecated (Not working)
+# is_slippery True (미끄러운 환경)
+env = gym.make('FrozenLake-v0')
+
+env.reset()
+#print_utils.clear_screen()
+env.render()  # Show the initial board
+
+while True:
+    # Choose an action from keyboard
+    key = readchar.readkey()
+
+    if key not in arrow_keys.keys():
+        print("Game aborted!")
+        break
+
+    action = arrow_keys[key]
+    state, reward, done, info = env.step(action)
+
+    # Show the board after action
+    print_utils.clear_screen()
+    env.render()
+
+    #print("State: ", state, "Action: ", action, "Reward: ", reward, "Info: ", info)
+    print("State: {} Action: {} Reward: {} Info: {}".format(state, action, reward, info))
+
+    if done:
+        #print("Finished with reward", reward)
+        print_utils.print_result(reward)
+        break
